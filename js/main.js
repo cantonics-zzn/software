6beb5b11dad441ed768e0a33f12c9e5a3d0a8563
cantonics <152353011+cantonics-zzn@users.noreply.github.com>
2026-07-12 16:56:34 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// 页面滚动平滑
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+console.log("投影显控网站加载完成");
